ad77a3411c2cc7ff2a7834a8a5f1e5914c719d3a
Константин Логиновских <kmloginovskih@sbercloud.ru>
2023-03-13 16:43:46 +0500

    invalidate query

--- src/examples/TargetTemperature/TargetTemperature.tsx
+++ src/examples/TargetTemperature/TargetTemperature.tsx
@@ -1,16 +1,21 @@
-import { useMutation, useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { fastServer } from "../../fakeServer";
 import { TargetTemperatureControl } from "../../ui";
 
 const server = fastServer;
 
 export const TargetTemperature = () => {
+  const queryClient = useQueryClient();
+
   const { data: temperature, isLoading } = useQuery("temperature", () =>
     server.get("temperature")
   );
 
   const { mutateAsync: setTemperature } = useMutation(
-    (nextTemperature: number) => server.put("temperature", nextTemperature)
+    (nextTemperature: number) => server.put("temperature", nextTemperature),
+    {
+      onSuccess: () => queryClient.invalidateQueries("temperature"),
+    }
   );
 
   if (isLoading) return <>Loading</>;
